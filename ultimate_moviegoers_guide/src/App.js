1e705ca2fcee40063940cab0ad7c0a1c3cb0ce1a
David Loveday <davemech@yahoo.com>
2019-02-01 02:22:14 -0800

    nicely getting movies from API

--- ultimate_moviegoers_guide/src/App.js
+++ ultimate_moviegoers_guide/src/App.js
@@ -1,26 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      movies: null,
+    }
+    this.performSearch("ant man")
+  } 
+  performSearch(searchTerm) {
+    const key = process.env.REACT_APP_SECRET;
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${key}=` + searchTerm)
+    .then(response =>{
+      console.log("Fire!", response)
+      const results = response.data.results
+      var moviesArray = []   
+      this.setState({movies: moviesArray})
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+  searchChangeHandler(event) {
+    console.log(event.target.value)
+    const boundObject = this
+    const searchTerm = event.target.value
+    boundObject.performSearch(searchTerm)
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="container ">        
+        <table className="titleBar">
+          <tbody>
+            <tr>
+              <td>
+                <img alt="app icon" width = "74px" src="green_app_icon.svg"/>
+              </td>
+              <td width="8"/>
+              <td>
+                <h1>Ultimate Moviegoers Guide</h1>
+              </td>
+            </tr>
+          </tbody>
+        </table>     
+        <input style={{
+          fontSize: 24,
+          display: 'block',
+          width: "23%",
+          paddingTop: 8,
+          paddingBottom: 8,
+          paddingLeft: 16,
+          borderRadius: '5px',     
+          
+        }} onChange={this.searchChangeHandler.bind(this)} placeholder="Search for a movie..."/>      
+        </div>     
     );
   }
 }
