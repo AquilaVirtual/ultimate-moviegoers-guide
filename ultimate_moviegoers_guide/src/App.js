07ef9f23f76b146f2126a3a9c2d693d8372d6751
David Loveday <davemech@yahoo.com>
2019-02-01 02:45:55 -0800

    add movie card

--- ultimate_moviegoers_guide/src/App.js
+++ ultimate_moviegoers_guide/src/App.js
@@ -1,62 +1,67 @@
-import React, { Component } from 'react';
-import './App.css';
-import axios from 'axios';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      movies: null,
-    }
-    this.performSearch("ant man")
-  } 
+      movies: null
+    };
+    this.performSearch("ant man");
+  }
   performSearch(searchTerm) {
     const key = process.env.REACT_APP_SECRET;
-    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${key}=` + searchTerm)
-    .then(response =>{
-      console.log("Fire!", response)
-      const results = response.data.results
-      var moviesArray = []   
-      this.setState({movies: moviesArray})
-    })
-    .catch(err => {
-      console.log(err)
-    })
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${key}=` + searchTerm
+      )
+      .then(response => {
+        console.log("Fire!", response);
+        const results = response.data.results;
+        var moviesArray = [];
+        this.setState({ movies: moviesArray });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
   searchChangeHandler(event) {
-    console.log(event.target.value)
-    const boundObject = this
-    const searchTerm = event.target.value
-    boundObject.performSearch(searchTerm)
+    console.log(event.target.value);
+    const boundObject = this;
+    const searchTerm = event.target.value;
+    boundObject.performSearch(searchTerm);
   }
   render() {
     return (
-      <div className="container ">        
+      <div className="container ">
         <table className="titleBar">
           <tbody>
             <tr>
               <td>
-                <img alt="app icon" width = "74px" src="green_app_icon.svg"/>
+                <img alt="app icon" width="74px" src="green_app_icon.svg" />
               </td>
-              <td width="8"/>
+              <td width="8" />
               <td>
                 <h1>Ultimate Moviegoers Guide</h1>
               </td>
             </tr>
           </tbody>
-        </table>     
-        <input style={{
-          fontSize: 24,
-          display: 'block',
-          width: "23%",
-          paddingTop: 8,
-          paddingBottom: 8,
-          paddingLeft: 16,
-          borderRadius: '5px',     
-          
-        }} onChange={this.searchChangeHandler.bind(this)} placeholder="Search for a movie..."/>      
-        </div>     
+        </table>
+        <input
+          style={{
+            fontSize: 24,
+            display: "block",
+            width: "23%",
+            paddingTop: 8,
+            paddingBottom: 8,
+            paddingLeft: 16,
+            borderRadius: "5px"
+          }}
+          onChange={this.searchChangeHandler.bind(this)}
+          placeholder="Search for a movie..."
+        />
+      </div>
     );
   }
 }
---
+++ ultimate_moviegoers_guide/src/MovieCard.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./MovieCard.css";
+import ChoreModal from "./ChoreModal";
+
+class MovieCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      open: false
+    };
+  }
+  handleModalToggle = () => {
+    this.setState({
+      open: !this.state.open
+    });
+
+    console.log("State of matter", this.state.open);
+  };
+  render() {
+    return (
+      <div className="post-card" key={this.props.movie.id}>
+        <img className="image" alt="poster" src={this.props.movie.poster_src} />
+        <div className="info">
+          <div className="title">{this.props.movie.title}</div>
+          <p className="overview">{this.props.movie.overview}...</p>
+        </div>
+        <p className="view_more" onClick={this.handleModalToggle}>
+          More Info
+        </p>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
